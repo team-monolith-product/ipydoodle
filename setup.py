55b2d7ba293d7b0211b4c4852b9e82f08b6062ca
BaeYeongbin <joybeen0403@gmail.com>
2022-05-25 03:51:47 +0000

    fix : 버전 정보 업데이트 및 ipycanvs 버전 고정

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="ipydoodle",
-    version="0.0.1",
+    version="0.1.1",
     # use the code below to enable extensions automatically
     install_requires=[
         'ipycanvas',
