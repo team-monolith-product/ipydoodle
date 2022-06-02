1d03c9d83fa49c4bf43eed24ab177d3051ed1d9f
BaeYeongbin <joybeen0403@gmail.com>
2022-06-02 12:52:39 +0900

    feat : 버전업 및 requires 추가

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
     name="ipydoodle",
-    version="0.1.1",
+    version="0.2.0",
     # use the code below to enable extensions automatically
     install_requires=[
-        'ipycanvas==0.12.0'
+        'ipycanvas==0.12.0',
+        'jupyter_ui_poll'
     ],
     packages=['ipydoodle'],
 )
